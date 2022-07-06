eacb527af7d97e9a05bb21a394c549a2b076e28f
gbavieira <gabrielattie@gmail.com>
2022-07-06 18:05:58 -0300

    About me Template

--- index/views.py
+++ index/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render (request, 'index.html')
+
+def about(request):
+    return render (request, 'about.html')
+
+def service(request):
+    return render (request, 'service.html')
+
+def projects(request):
+    return render (request, 'projects.html')
